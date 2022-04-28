396088316b22d6ce6b1a633ab72815ede0b3cd2e
Zicklag <zicklag@katharostech.com>
2022-04-28 11:59:40 -0500

    Finish Hello World!

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello world!";
+static HELLO: &[u8] = b"Hello world! Welcome to Blog OS!";
 
 const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
 
