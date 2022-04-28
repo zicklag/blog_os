1d65983853fec53115d5751f6754f44a57a860b5
Zicklag <zicklag@katharostech.com>
2022-04-28 10:02:55 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello world!";
+
+const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *VGA_BUFFER.offset(i as isize * 2) = byte;
+            *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
